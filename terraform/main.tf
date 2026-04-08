6947f8e9cfb6fb50b15d00e6dded160dcdcac8d6
Carissa Co <chco@dons.usfca.edu>
2026-04-07 23:53:27 -0700

    rewrite main.tf to use vpc module rather than hand-written module

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,25 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
-  project_name         = var.project_name
-  vpc_cidr             = "10.0.0.0/16"
-  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
-  availability_zones   = ["us-east-1a", "us-east-1b"]
+  name = "${var.project_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Project = var.project_name
+  }
 }
 
 module "ec2" {
@@ -19,6 +31,6 @@ module "ec2" {
   vpc_id             = module.vpc.vpc_id
   ami_id             = var.ami_id
   my_ip              = var.my_ip
-  public_subnet_id   = module.vpc.public_subnet_ids[0]
-  private_subnet_ids = module.vpc.private_subnet_ids
+  public_subnet_id   = module.vpc.public_subnets[0]
+  private_subnet_ids = module.vpc.private_subnets
 }
